dd695623391fd7c9f250bdba5086a9621e80bcb8
CairMK <zbogoevski@gmail.com>
2018-11-06 11:42:50 +0100

    fix self generate meny bug

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,62 +2,63 @@
 
 namespace App\Models;
 
-class Category extends \Baum\Node
-{
+class Category extends \Baum\Node {
 
-    public $timestamps = false;
-    protected $fillable = array('name', 'parent_id', 'slug', 'file', 'filename', 'image', 'description');
+	public $timestamps = false;
+	protected $fillable = array('name', 'parent_id', 'slug', 'file', 'filename', 'image', 'description');
 
-    public static function getTree($categories)
-    {
+	public static function getTree($categories) {
 
-        $lists = '<ul class="dropdown">';
-        foreach ($categories as $category)
-            $lists .= self::renderNode($category);
-        $lists .= '</ul>';
-        return $lists;
-    }
+		$lists = '<ul>';
+		foreach ($categories as $category) {
+			$lists .= self::renderNode($category);
+		}
 
-    public static function renderNode($node)
-    {
-        $list = '<li><a href="/admin/categories/' . $node->id . '/edit">' . $node->name;
-        if ($node->children()->count() > 0) {
-            $list .= '<ul>';
-            foreach ($node->children as $child)
-                $list .= self::renderNode($child);
-            $list .= "</ul>";
-        }
+		$lists .= '</ul>';
+		return $lists;
+	}
 
-        $list .= "</a></li>";
-        return $list;
-    }
+	public static function renderNode($node) {
+		$list = '<li><a href="/admin/categories/' . $node->id . '/edit">' . $node->name;
+		if ($node->children()->count() > 0) {
+			$list .= '<ul>';
+			foreach ($node->children as $child) {
+				$list .= self::renderNode($child);
+			}
 
-    public static function getTreeHP($categories)
-    {
-        $lists = '<li class="dropdown">';
-        foreach ($categories as $category)
-            $lists .= self::renderNodeHP($category);
-        $lists .= '</li>';
-        return $lists;
-    }
+			$list .= "</ul>";
+		}
 
-    public static function renderNodeHP($node)
-    {
-        $list = '<li class="dropdown"><a href="/categories/' . $node->slug . '" class="nav-link dropdown-toggle" id="fifth-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' . $node->name . '</a>';
-        if ($node->children()->count() > 0) {
-            $list .= '<ul class="dropdown-menu" aria-labelledby="fifth-dropdown">';
-            foreach ($node->children as $child)
-                $list .= self::renderNodeHP($child);
-            $list .= "</ul>";
-        }
+		$list .= "</a></li>";
+		return $list;
+	}
 
-        $list .= "</li>";
-        return $list;
-    }
+	public static function getTreeHP($categories) {
+		$lists = '';
+		foreach ($categories as $category) {
+			$lists .= self::renderNodeHP($category);
+		}
 
-    public function product()
-    {
-        return $this->belongsTo('App\Models\Post', 'category');
-    }
+		$lists .= '';
+		return $lists;
+	}
+
+	public static function renderNodeHP($node) {
+		$list = '<li class="drop-down"><a href="/categories/' . $node->slug . '" >' . $node->name . '</a>';
+		if ($node->children()->count() > 0) {
+			$list .= '<ul class="drop-down-menu drop-down-inner">';
+			foreach ($node->children as $child) {
+				$list .= self::renderNodeHP($child);
+			}
+
+			$list .= "</ul>";
+		}
+
+		$list .= "</li>";
+		return $list;
+	}
+
+	public function product() {
+		return $this->belongsTo('App\Models\Product', 'category');
+	}
 }
-
